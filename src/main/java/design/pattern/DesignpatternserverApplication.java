92f7ce775e33f159cc5102277c9621abafca320c
Adarsh Sharma <adarsh2803@gmail.com>
2023-10-26 03:45:08 +0530

    first commit of springboot

---
+++ src/main/java/design/pattern/DesignpatternserverApplication.java
@@ -0,0 +1,14 @@
+package design.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternserverApplication {
+
+	public static void main(String[] args) {
+		System.out.println("this finally run");
+		SpringApplication.run(DesignpatternserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/design/pattern/DesignpatternserverApplicationTests.java
@@ -0,0 +1,10 @@
+package design.pattern;
+
+
+class DesignpatternserverApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
