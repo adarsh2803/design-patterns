986d405e1755c30b9ae4ecd8f6cfdb53f72f1625
Adarsh Sharma <adarsh2803@gmail.com>
2023-11-01 23:55:23 +0530

    ProxyDesignPattern added

---
+++ src/main/java/com/design/pattern/designPatterns/structuralDesignPattern/ProxyDesignPattern.java
@@ -0,0 +1,53 @@
+package com.design.pattern.designPatterns.structuralDesignPattern;
+
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String filename;
+
+    public RealImage(String filename) {
+        this.filename = filename;
+        loadFromDisk();
+    }
+
+    private void loadFromDisk() {
+        System.out.println("Loading image: " + filename);
+    }
+
+    public void display() {
+        System.out.println("Displaying image: " + filename);
+    }
+}
+
+class ImageProxy implements Image {
+    private RealImage realImage;
+    private String filename;
+
+    public ImageProxy(String filename) {
+        this.filename = filename;
+    }
+
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(filename);
+        }
+        realImage.display();
+    }
+}
+
+public class ProxyDesignPattern {
+    public static void main(String[] args) {
+        Image image1 = new ImageProxy("image1.jpg");
+        Image image2 = new ImageProxy("image2.jpg");
+
+        // Images are loaded only when displayed
+        image1.display();
+        image2.display();
+
+        //image not reloaded
+        image1.display();
+        image2.display();
+    }
+}
