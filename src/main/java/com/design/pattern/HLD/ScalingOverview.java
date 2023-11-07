52e7c4ac3a6ce70953401aee351d50dc1be4fd5b
Adarsh Sharma <adarsh2803@gmail.com>
2023-11-08 00:30:06 +0530

    ScalingOverview added

---
+++ src/main/java/com/design/pattern/HLD/ScalingOverview.java
@@ -0,0 +1,18 @@
+package com.design.pattern.HLD;
+
+public class ScalingOverview {
+    /*
+        Steps involved in scaling system from 0 to MILLION users
+        1. Single Server
+        2. Application and DB server separation
+        3. Load Balancer + Multiple App servers
+        4. DataBase Replication
+        5. CACHE
+        6. CDN
+        7. Data Centre
+        8. Messaging Queues
+        9. DataBase Scaling
+
+
+     */
+}
